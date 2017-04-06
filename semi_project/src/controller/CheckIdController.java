d295a74c8fef035f335410e7c986231909669b5a
Administrator <Administrator@MSDN-SPECIAL>
2017-04-06 20:29:10 +0900

    checkId controller

--- semi_project/src/controller/CheckIdController.java
+++ semi_project/src/controller/CheckIdController.java
@@ -1,5 +1,7 @@
 package controller;
 
+import java.io.PrintWriter;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
@@ -10,14 +12,21 @@ public class CheckIdController implements Controller {
 	@Override
 	public String execute(HttpServletRequest request, HttpServletResponse response) throws Exception {
 		String userId = request.getParameter("id");
-		System.out.println(userId);
 		
 		// ID 중복 체크해서 불린형으로
-		String flag = MemberDAO.getInstance().searchId(userId);
+		boolean flag = MemberDAO.getInstance().searchId(userId);
 		// flag -> true, false
+		PrintWriter out=response.getWriter();
 		
+		if(flag == true){
+			out.print("ok"); //사용가능
+		}else{
+			out.print("fail");// 사용불가
+		}
 		
-		return flag;
+		out.close();
+		
+		return "AjaxView";
 	}
 
 }
